1bc3c2699692203ac727fb14fd6f9a623c97a411
uvacoder <uva22940coder@gmail.com>
2022-11-27 14:02:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# P5 ML PoseNet Start Kit
+
+This repository uses [p5.js](https://p5js.org), [ml5.js](https://ml5js.org), and
+[PoseNet](https://github.com/tensorflow/tfjs-models/tree/master/posenet) to
+display a webcam image, labelled with pose detection skeleton and points.
+
+## Installation
+
+1. Clone this repo.
+
+2. In Visual Studio Code, install the [Live Server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer) extensions.
+
+## Running
+
+In Visual Studio Code's status bar, click on "Go Live".
+
+This will open PoseNet in a browser window.
+
+## License
+
+MIT License
---
+++ sketch.js
@@ -0,0 +1,52 @@
+let video; // setup initializes this to a p5.js Video instance.
+let poses = []; // the poseNet.on callback sets this from new poses
+
+function setup() {
+  createCanvas(640, 480);
+  video = select("video") || createCapture(VIDEO);
+  video.size(width, height);
+
+  const poseNet = ml5.poseNet(video, { flipHorizontal: true }, () => {
+    select("#status").hide();
+  });
+
+  poseNet.on("pose", (newPoses) => {
+    poses = newPoses;
+  });
+
+  // Hide the video element, and just show the canvas
+  video.hide();
+}
+
+function draw() {
+  push();
+  translate(video.width, 0);
+  scale(-1, 1);
+  image(video, 0, 0);
+  pop();
+
+  drawKeypoints(poses);
+  drawSkeleton(poses);
+}
+
+function drawKeypoints(poses) {
+  for (let pose of poses) {
+    for (let keypoint of pose.pose.keypoints) {
+      if (keypoint.score > 0.2) {
+        fill(0, 255, 0);
+        noStroke();
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+      }
+    }
+  }
+}
+
+function drawSkeleton(poses) {
+  for (let pose of poses) {
+    for (let skeleton of pose.skeleton) {
+      const [p1, p2] = skeleton;
+      stroke(255, 0, 0);
+      line(p1.position.x, p1.position.y, p2.position.x, p2.position.y);
+    }
+  }
+}
